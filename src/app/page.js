759ad2409cabe6c3a0b12e754dd625044028fb96
Bablu <singhbablukumar279@gmail.com>
2024-04-10 06:47:14 +0530

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,20 @@
 'use client'
 
+import { useState } from "react"
+
 
 export default function Home() {
-  
+  const[count,setCount]=useState(0)
+  const handlebuttonclick=()=>{
+    const updatedvolume=count + 1
+    setCount(updatedvolume)
+  }
  return(
   <>
    
-    hello how are these page
-    new code from here
-    <h1 className=" text-red-400">tailwind css</h1>
+   
+    <h1 className=" flex justify-center items-center font-extrabold text-blue-400">volume{count}</h1>
+    <button className="flex justify-center items-center font-extrabold text-green-400" onClick={handlebuttonclick}>level up</button>
   </>
  )
 }
